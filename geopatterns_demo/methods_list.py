215de7c8554c533255aebe541b915961c4863cba
RomanZorkin <zorkin.roman@gmail.com>
2021-08-03 21:59:43 +0700

    1. Fixed errors in accordance with the requirements of linters

--- geopatterns_demo/methods_list.py
+++ geopatterns_demo/methods_list.py
@@ -4,6 +4,6 @@ from geopatterns_demo.models import Method
 def describe_available_methods():
     """Generate list of the methods geopatterns supports."""
     return [
-        {'name': name, 'label': label}
-        for name, label in Method.__members__.items()
+        {'name': model.name, 'label': model.value}
+        for model in Method
     ]
