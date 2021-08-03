a8f67e902354a02abd901c6633932846ec0b6d55
RomanZorkin <zorkin.roman@gmail.com>
2021-08-03 21:07:49 +0700

    1. The name of the method has been adjusted to "describe_available_methods" 2. The list is formed using "list comprehension" method 3. There is an error (WPS609) in the process of applying "flake8". The error warns about the use of the hidden attribute __members__ of the Enum library, and suggests using the super () method. The library documentation assumes the use of this attribute in projects "The __members__ attribute can be used for detailed programmatic access to the enumeration members. For example, finding all the aliases"

--- geopatterns_demo/methods_list.py
+++ geopatterns_demo/methods_list.py
@@ -1,13 +1,9 @@
 from geopatterns_demo.models import Method
 
-def list_creator():
+
+def describe_available_methods():
     """Generate list of the methods geopatterns supports."""
-    methods_list = []
-    for name, label in Method.__members__.items():
-        methods_list.append(
-            {
-                'name': name,
-                'label': label
-            }
-        )
-    return methods_list
+    return [
+        {'name': name, 'label': label}
+        for name, label in Method.__members__.items()
+    ]
