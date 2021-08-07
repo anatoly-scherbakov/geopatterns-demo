9ed6e02847f042210093463bbbe1f0cf9435da20
RomanZorkin <zorkin.roman@gmail.com>
2021-08-07 19:20:40 +0700

    Issue#9 correct random_phrase.py
    1. Function make_random_text returns only text(str)

--- geopatterns_demo/random_phrase.py
+++ geopatterns_demo/random_phrase.py
@@ -3,4 +3,4 @@ import loremipsum
 
 def make_random_text():
     """Method create random phrase using loremipsum library."""
-    return {'text': loremipsum.generate(1, 'short')}
+    return loremipsum.generate(1, 'short')
