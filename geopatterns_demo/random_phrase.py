89ca7386794a62cd05fbd3d2579c2a65327b19d0
RomanZorkin <zorkin.roman@gmail.com>
2021-08-17 21:42:57 +0700

    Issue #23 Our own text generator
    In module random_phrase.py
    1. added comment  about the exception in linter test
    2. fixed the variable name in the make_random_text function

--- geopatterns_demo/random_phrase.py
+++ geopatterns_demo/random_phrase.py
@@ -13,10 +13,13 @@ class MakeWord():
         """Find random word in text file."""
         with open(self.text_file, 'r') as text:
             lines = text.readlines()
-            return random.choice(lines).rstrip('\n')
+            # random.choice() is considered to be insecure by flake8-bandit,
+            # but we're not using this for any kind of cryptographic
+            # or security related purpose.
+            return random.choice(lines).rstrip('\n') # noqa: S311
 
 
 def make_random_text():
     """Glues  and return a phrase from random words."""
-    words_list = [MakeWord(word.value).random_word() for word in Thesaurus]
+    words_list = [MakeWord(file_path.value).random_word() for file_path in Thesaurus]
     return str(' '.join(words_list))
