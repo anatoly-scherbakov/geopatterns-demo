f2a1a324a8af4d826274fef16e331db4f9730750
RomanZorkin <71222100+RomanZorkin@users.noreply.github.com>
2021-11-14 10:12:19 +0700

    Adjustments have been made according to the comments
    Changes have been made in accordance with https://github.com/anatoly-scherbakov/geopatterns-demo/pull/32#pullrequestreview-797642871

--- geopatterns_demo/file_handler.py
+++ geopatterns_demo/file_handler.py
@@ -6,67 +6,34 @@ from geopatterns_demo import models, settings
 from geopatterns_demo.draw import geopattern
 
 
-def hash_name(text: str) -> str:
-    """Generate the file name using the hash function.
-
-    Args:
-        text (str): text that we get from get request
-
-    Returns:
-        str: file name
-    """
-    return 'image{0}.png'.format(hash(text))
-
-
-def check_file(file_name: str, bucket: boto3) -> bool:
-    """Check the file for presence in the S3 bucket.
-
-    Args:
-        file_name (str): image file name
-        bucket (boto3): boto3 object - S3 bucket
-
-    Returns:
-        bool: bool
-    """
-    bucket_object = list(bucket.objects.filter(Prefix=file_name))
-    if len(bucket_object) > 0:  # noqa: WPS507
-        return bucket_object
-
-
 def file_to_s3(file_name: str, image: BytesIO) -> str:
-    """Save file to S3 bucket & return it's URL.
-
+    """Save file to S3 bucket & return its URL.
     Args:
         file_name (str): image file name
         image (BytesIO): image object in bytes
-
     Returns:
         str: url
     """
     s3 = boto3.Resource('s3')
     bucket = s3.Bucket(settings.S3_BUCKET_NAME)
     bucket.upload_fileobj(image.read(), file_name)
-    if check_file(file_name, bucket):
-        return 'http://s3-{0}-.amazonaws.com/{1}/{2}'.format(
-            settings.AWS_REGION,
-            settings.S3_BUCKET_NAME,
-            file_name,
-        )
-    return 'error: file is not saving.'
+    return 'http://s3-{0}-.amazonaws.com/{1}/{2}'.format(
+        settings.AWS_REGION,
+        settings.S3_BUCKET_NAME,
+        file_name,
+    )
 
 
 def get_url(text: str, method: models.Method) -> str:
     """Generate Geopatterns file & and starts the procedure for saving it.
-
     Args:
         text (str): text that we get from get request
         method: method for generate Geopatterns
-
     Returns:
         str: url
     """
     return file_to_s3(
-        hash_name(text),
+        'image{0}.png'.format(hash(text)),
         image=geopattern(
             text=text,
             method=method,
